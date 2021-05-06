63138ded0473bf2aa90455f77b9725a89ad4b7df
LartisantCodeur <79115307+LartisantCodeur@users.noreply.github.com>
2021-05-06 13:42:41 +0200

    V0.5.14

--- vue/compilatedVue.php
+++ vue/compilatedVue.php
@@ -26,6 +26,12 @@
         <button id="buttonExit" onclick="window.location.href = '.'">
             <img id="buttonExitIco" src="images/ico_exit.png">
         </button>
+		<button id="buttonAdd">
+			<img id="buttonAddIco" src="images/ico_add.png">
+		</button>
+		<button id="buttonSearch">
+			<img id="buttonSearchIco" src="images/ico_search.png">
+		</button>
         <div id="actionSelectButtonFrame">
             <button class="buttonAction" onclick="selectAction(this);">Retrait</button>
             <button class="buttonAction" onclick="selectAction(this);">Dépôt</button>
