e877f4b4736a40e6cf31835ba257853fe17effa6
LartisantCodeur <79115307+LartisantCodeur@users.noreply.github.com>
2021-05-07 09:10:33 +0200

    V0.7.1

--- vue/waitVue.php
+++ vue/waitVue.php
@@ -2,9 +2,15 @@
 <html>
 <head>
 	<title></title>
+	<link href="CSS/wait.css" rel="stylesheet" />
 </head>
 <body>
-<h1>Commande réussi.</h1><br><h2>Vous allez être redirigé dans 5 secondes</h2>
+<div id="text">
+	<h1>Commande réussi.</h1><br><h2>Vous allez être redirigé dans 5 secondes</h2>
+	<div id="logo">
+		\(^-^)/
+	</div>
+</div>
 <meta http-equiv="refresh" content = "5; url = ." >
 </body>
 </html>
